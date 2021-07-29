2ddf7263093a95cf4b60dff3be754178c1e52f4f
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-29 22:46:56 +0100

    added heroku database

---
+++ config/seroku.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: 'ec2-3-212-75-25.compute-1.amazonaws.com',
+  PORT: '5432',
+  USER: 'glaokuqwfxoxgn',
+  PASSWORD: '87b02165a1930df06185728c0256f38b792b0f1b8af3369cdc26edf0c7773f68',
+  DB: 'd9f9h1b6eii55m',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000,
+    idle: 1000
+  }
+}
