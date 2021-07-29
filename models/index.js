0251909045309299f795ab58fabac639c050eb55
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-29 21:04:59 +0100

    renamed product from post_id

--- models/index.js
+++ models/index.js
@@ -42,7 +42,7 @@ db.comments = require('../models/comment.models')(sequelize, Sequelize)
 db.rating = require('../models/rating.models')(sequelize, Sequelize)
 
 db.user.hasMany(db.post, { foreignKey: 'user_id' })
-db.post.hasMany(db.comments, { foreignKey: 'product_id' })
-db.post.hasMany(db.rating, { foreignKey: 'product_id' })
+db.post.hasMany(db.comments, { foreignKey: 'post_id' })
+db.post.hasMany(db.rating, { foreignKey: 'post_id' })
 
 module.exports = db
