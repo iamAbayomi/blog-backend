aecf76664a1229403ece50aa922430141a2572af
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-29 16:51:07 +0100

    install mysql2

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+// Init body-parser options (inbuilt with express)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// Initializing Database
+const db = require('./models')
+db.sequelize.sync({ alter: true })
+
+
 app.get('/', (req, res) => {
     res.send({
-        "message": 'Fola is crazy'
+        "message": 'Fola is crazyyling good'
     })
 })
 
+require('./routes/user.routes')(app)
+require('./routes/post.routes')(app)
+require('./routes/comment.routes')(app)
+require('./routes/rating.routes')(app)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
