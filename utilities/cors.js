2cb4afa1612e2fc7312c8a8b0e53bd577189a7b6
iamAbayomi <oladinifolawiyo>
2022-06-18 05:05:04 +0100

    refactor: allowed cors from all origins

--- utilities/cors.js
+++ utilities/cors.js
@@ -4,17 +4,20 @@ module.exports = (app) => {
   const allowedOrigins = ['http://localhost:3000','http://localhost:4000', 'https://a-blog-in-react.netlify.app',"*" ,
     'https://spiinge.com', 'https://www.spiinge.com', 'https://spiinge.appspot.com/']
 
-  app.use(cors({
-    origin (origin, callback) {
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-      if (!origin) { return callback(null, true) }
-      if (!allowedOrigins.includes(origin)) {
-        const msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.' + origin
-        return callback(new Error(msg), false)
-      }
-      return callback(null, true)
-    }
-  }))
+  // app.use(cors({
+  //   origin (origin, callback) {
+  //   // allow requests with no origin
+  //   // (like mobile apps or curl requests)
+  //     if (!origin) { return callback(null, true) }
+  //     if (!allowedOrigins.includes(origin)) {
+  //       const msg = 'The CORS policy for this site does not ' +
+  //               'allow access from the specified Origin.' + origin
+  //       return callback(new Error(msg), false)
+  //     }
+  //     return callback(null, true)
+  //   }
+  // }))
+
+  app.use(cors())
+
 }
